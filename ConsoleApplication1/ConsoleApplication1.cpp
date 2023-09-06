db2b9c95d9955924d84f3baa7af4eacf8b75d11d
Edgar Lavrinenko <destrxlarie@gmail.com>
2023-09-06 20:41:33 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,50 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+	char operand;
+	double first;
+	double second;
+	string res = "Result is: ";
+	cout << "Enter first number: ";
+	cin >> first;
+	cout << "Enter second number: ";
+	cin >> second;
+	cout << "Enter operand: ";
+	cin >> operand;
+	switch (operand)
+	{
+	case '+':
+		cout << res << first + second << endl;
+		break;
+
+	case '-':
+		cout << res << first - second << endl;
+		break;
+
+	case '*':
+		cout << res << first * second << endl;
+		break;
+	case'/':
+		cout << res << first / second << endl;
+		break;
+
+	default: 
+		cout << "Unknown operation" << endl;
+		break;
+
+	}
+	
+	
+	
+
+	
+
+
+    
+
+}
---
+++ ConsoleApplication1/СalculatorC++.cpp
@@ -0,0 +1,49 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char operand;
+	double first;
+	double second;
+	string res = "Result is: ";
+	cout << "Enter first number: ";
+	cin >> first;
+	cout << "Enter second number: ";
+	cin >> second;
+	cout << "Enter operand: ";
+	cin >> operand;
+	switch (operand)
+	{
+	case '+':
+		cout << res << first + second << endl;
+		break;
+
+	case '-':
+		cout << res << first - second << endl;
+		break;
+
+	case '*':
+		cout << res << first * second << endl;
+		break;
+	case'/':
+		cout << res << first / second << endl;
+		break;
+
+	default: 
+		cout << "Unknown operation" << endl;
+		break;
+
+	}
+	
+	
+	
+
+	
+
+
+    
+
+}
